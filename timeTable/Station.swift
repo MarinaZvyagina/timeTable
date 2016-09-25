0f067749b2b83bf0236b90e65b4affae8555d952
Марина Звягина <zvyagina.marina@gmail.com>
2016-09-25 04:34:51 +0300

    getStationFromJson Method to form Station From AnyObject and Test for this method Added forgotten field

--- timeTable/Station.swift
+++ timeTable/Station.swift
@@ -12,21 +12,46 @@ class Station {
     let countryTitle: String
     let point: (longitude: Double, latitude: Double)
     let districtTitle: String
+    let cityId: Int
     let cityTitle: String
     let regionTitle: String
     let stationId: Int
     let stationTitle: String
     
-    init(countryTitle: String, point: (longitude: Double, latitude: Double), districtTitle: String, cityTitle: String, regionTitle: String, stationId: Int, stationTitle: String)
+    init(countryTitle: String, point: (longitude: Double, latitude: Double), districtTitle: String, cityId: Int, cityTitle: String, regionTitle: String, stationId: Int, stationTitle: String)
     {
         self.countryTitle = countryTitle
         self.point = point
         self.districtTitle = districtTitle
+        self.cityId = cityId
         self.cityTitle = cityTitle
         self.regionTitle = regionTitle
         self.stationId = stationId
         self.stationTitle = stationTitle
     }
     
+    static func getStationFromJson(station:AnyObject)->Station{
+        let countryTitle = station["countryTitle"] as! String
+        
+        let pointFromStation = station["point"] as AnyObject!
+        let pointLongitude = pointFromStation["longitude"] as! Double
+        let pointLatitude = pointFromStation["latitude"] as! Double
+        let point = (pointLongitude,pointLatitude)
+        
+        let districtTitle = station["districtTitle"] as! String
+        
+        let cityId = station["cityId"] as! Int
+        
+        let cityTitle = station["cityTitle"] as! String
+        
+        let regionTitle = station["regionTitle"] as! String
+        let stationId = station["stationId"] as! Int
+        let stationTitle = station["stationTitle"] as! String
+
+        let station = Station(countryTitle: countryTitle, point: point, districtTitle: districtTitle, cityId: cityId, cityTitle: cityTitle, regionTitle: regionTitle, stationId: stationId, stationTitle: stationTitle)
+        
+        return station
+        
+    }
     
 }
---
+++ timeTableTests/StationTests.swift
@@ -0,0 +1,48 @@
+//
+//  StationTests.swift
+//  timeTable
+//
+//  Created by Марина Звягина on 25.09.16.
+//  Copyright © 2016 Марина Звягина. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import timeTable
+
+class StationTests: XCTestCase {
+    
+    func testGetStationFromJson() {
+       // let jsonString = "{\"name\":\"John\",\"age\":32,\"phoneNumbers\":[{\"type\":\"home\",\"number\":\"212 555-1234\"}]}"
+        
+        
+        let jsonString="{\"countryTitle\":\"Австрия\",\"point\":{\"longitude\":16.36879539489746,\"latitude\":48.20253753662109},\"districtTitle\":\"\",\"cityId\":2352,\"cityTitle\":\"город Вена\",\"regionTitle\":\"\",\"stationId\":9778,\"stationTitle\":\"Stadion Center, Olympiaplatz 2\"}"
+    
+        let jsonData = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        var jsonObject: AnyObject! = nil
+            
+        do {
+           jsonObject = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions())
+        } catch {
+            print(error)
+        }
+        
+        let result = Station.getStationFromJson(jsonObject)
+        
+        XCTAssertEqual(result.countryTitle, "Австрия")
+        XCTAssertEqual(result.point.longitude, 16.36879539489746)
+        XCTAssertEqual(result.point.latitude, 48.20253753662109)
+
+        XCTAssertEqual(result.districtTitle, "")
+        XCTAssertEqual(result.cityId, 2352)
+        XCTAssertEqual(result.cityTitle, "город Вена")
+        XCTAssertEqual(result.regionTitle, "")
+        XCTAssertEqual(result.stationId, 9778)
+        XCTAssertEqual(result.stationTitle, "Stadion Center, Olympiaplatz 2")
+        
+    }
+    
+    
+}
+
